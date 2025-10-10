942df6ed68814d69bbf20ff1fe5935aef9471964
Марк Антонов <dbasbkfga@Marks-MacBook-Air.local>
2025-10-10 14:13:09 +0200

    ыущ гзвфеу

--- script.js
+++ script.js
@@ -1,27 +1,25 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     new Glide('#glide1').mount();
     new Glide('#glide2').mount();
     new Glide('#glide3').mount();
+    new Glide('#glide4').mount();
+
+    updateThemeBasedOnScroll(); // запускаем сразу при загрузке
 });
 
-window.addEventListener('scroll', function() {
-    var targetScrollPosition = 600; // Задайте желаемую позицию прокрутки, при которой должен измениться цвет фона и текста
-    var currentScrollPosition = window.scrollY;
+window.addEventListener('scroll', function () {
+    updateThemeBasedOnScroll();
+});
 
-    if (currentScrollPosition >= targetScrollPosition) {
-        document.body.style.backgroundColor = '#E8E8DF'; // Задайте желаемый цвет фона
-        document.body.style.color = 'black'; // Задайте желаемый цвет текста
-        setLinkColor('black'); // Задайте цвет ссылок, который будет до достижения желаемой позиции
+function updateThemeBasedOnScroll() {
+    const scrollY = window.scrollY;
+    const body = document.body;
+
+    if (scrollY >= 600) {
+        body.classList.add('light-theme');
+        body.classList.remove('dark-theme');
     } else {
-        document.body.style.backgroundColor = 'black'; // Задайте цвет фона, который будет до достижения желаемой позиции
-        document.body.style.color = '#E8E8DF'; // Задайте цвет текста, который будет до достижения желаемой позиции
-        setLinkColor('white'); // Задайте желаемый цвет ссылок
-    }
-});
-
-function setLinkColor(color) {
-    var links = document.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        links[i].style.color = color;
+        body.classList.add('dark-theme');
+        body.classList.remove('light-theme');
     }
 }
